0f2741db248343d779728b060d61650569d8cee9
enlian <frejaenlian@gmail.com>
2024-10-27 16:12:46 +0900

    fix bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ const backendUrl =
     ? "https://react-cms.onrender.com" // 生产环境后端地址
     : "http://localhost:3000"; // 开发环境后端地址
 
-console.log("Vite 配置加载成功", backendUrl);
+console.log("Vite 配置加载成功", backendUrl,process.env.NODE_ENV);
 
 export default defineConfig({
   plugins: [react()],
