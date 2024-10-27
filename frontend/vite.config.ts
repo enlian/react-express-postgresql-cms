da5fd3789371c4b6130f6ffad20d39913e29ed52
enlian <frejaenlian@gmail.com>
2024-10-27 16:12:18 +0900

    fix bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // 设置后端地址，区分开发和生产环境
 const backendUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://react-cms.onrender.com' // 生产环境后端地址
-    : 'http://localhost:3000'; // 开发环境后端地址
+  process.env.NODE_ENV === "production"
+    ? "https://react-cms.onrender.com" // 生产环境后端地址
+    : "http://localhost:3000"; // 开发环境后端地址
+
+console.log("Vite 配置加载成功", backendUrl);
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      "/api": {
         target: backendUrl, // 根据环境动态设置目标地址
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''), // 可选：重写路径
