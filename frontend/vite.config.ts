ed032737a34689fbdbe8247adc3cca1523e18f00
enlian <frejaenlian@gmail.com>
2024-11-05 00:47:14 +0900

    fix bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // 设置后端地址，区分开发和生产环境
 const backendUrl =
   process.env.NODE_ENV === "production"
-    ? "https://54.199.180.45" // 生产环境后端地址
+    ? "https://54.199.180.45:3000" // 生产环境后端地址
     : "http://localhost:3000"; // 开发环境后端地址
 
 console.log("Vite 配置加载成功", backendUrl, process.env.NODE_ENV);
